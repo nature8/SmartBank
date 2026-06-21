eb1226480ea9e53c455391d8861bf156a9cb2113
Prakruti Tailor <50707451+nature8@users.noreply.github.com>
2026-06-21 20:43:30 +0530

    CustomerService Added

---
+++ CustomerService/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomerService.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        // Comes from Auth Service
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
